d6845ee6a1b2b10326b2a4f8c3b7781511cf85f3
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2022-01-05 01:19:00 -0300

    fix: Clean input after add list item.

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 var idItem=0;
 
 function addItemList(){
-    let content = document.getElementById("newListContent").value;
+    let content = document.getElementById("newListContent");
     let listToDo = document.getElementById("todoList");
     let checkboxName = `chkItem${idItem}`;
-    listToDo.insertAdjacentHTML("beforeend",`<div class="item animate__animated animate__bounceIn"><input type="checkbox" name="${checkboxName}" id="${checkboxName}" class="todoCheckbox"><label for="${checkboxName}">${content}</label></div>`);
+    listToDo.insertAdjacentHTML("beforeend",`<div class="item animate__animated animate__bounceIn"><input type="checkbox" name="${checkboxName}" id="${checkboxName}" class="todoCheckbox"><label for="${checkboxName}">${content.value}</label></div>`);
+    content.value = '';
     idItem++;
 }
 
